9a53d36ccd3b1cbdb07706b2004c8b3ee3d5dbed
shodai <pippi9669@yahoo.co.jp>
2023-09-02 11:41:14 +0900

    すこしできた

--- scraping.py
+++ scraping.py
@@ -1,61 +1,59 @@
-# import requests
-# from bs4 import BeautifulSoup
-
-# url = "https://www.sejuku.net/blog/"
-
-# response = requests.get(url)
-# response.encoding = response.apparent_encoding
-
-# bs = BeautifulSoup(response.text, 'html.parser')
-
-# for i in bs.select("h3"):
-#     print(i.getText())
-
-# coding: UTF-8
-
-# import requests
-# from bs4 import BeautifulSoup
-
-# url = "https://www.fujitv.co.jp/meza/uranai/index.html"
-
-# # WEBサイトからHTMLデータを取得
-# response = requests.get(url)
-
-# if response.status_code == 200:
-#     # BeautifulSoupオブジェクトを作成
-#     soup = BeautifulSoup(response.content, 'html.parser')
-    
-#     # タイトルを取得して表示
-#     title = soup.title.text.strip()
-#     print("タイトル:", title)
-    
-#     # 本文の一部を表示
-#     content = soup.find('div', class_='section-inner').text.strip()
-#     print("本文:", content)
-# else:
-#     print("WEBサイトにアクセスできませんでした。")
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.fujitv.co.jp/meza/uranai/index.html"
+# スクレイピング対象の URL にリクエストを送り HTML を取得する
+# LINE占い
+resLINE = requests.get('https://fortune.line.me/contents/horoscope/')
+# Dmenu占い
+# resDmenu = requests.get('https://service.smt.docomo.ne.jp/portal/fortune/src/fortune_ranking.html')
+# 占いの泉
+resIzumi = requests.get('https://izumi.uranai.jp/unsei')
+# 家庭画報
+res家庭画報 = requests.get('https://www.kateigaho.com/horoscope/')
+# Rsk
+resRsk = requests.get('https://www.rsk.co.jp/horoscope/')
+# ハルメク365
+resハルメク365 = requests.get('https://halmek.co.jp/culture/c/psychology/daily')
+# 朝日新聞占い
+res朝日新聞 = requests.Session().get('https://www.asahi.com/uranai/12seiza/')
+res朝日新聞.encoding = res朝日新聞.apparent_encoding
 
-# WEBサイトからHTMLデータを取得
-response = requests.get(url)
 
-if response.status_code == 200:
-    # BeautifulSoupオブジェクトを作成
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # タイトルを取得して表示
-    title = soup.title.text.strip()
-    print("タイトル:", title)
-    
-    # class="result1"の中身を取得して表示
-    result1_element = soup.find('div', class_='result1')
-    if result1_element:
-        result1_content = result1_element.text.strip()
-        print("class=\"result1\"の中身:", result1_content)
-    else:
-        print("class=\"result1\"の要素が見つかりませんでした。")
-else:
-    print("WEBサイトにアクセスできませんでした。")
+# レスポンスの HTML から BeautifulSoup オブジェクトを作る
+soupLINE = BeautifulSoup(resLINE.text, 'html.parser')
+# soupDmenu = BeautifulSoup(resDmenu.text, 'html.parser')
+soupIzumi = BeautifulSoup(resIzumi.text, 'html.parser')
+soup家庭画報 = BeautifulSoup(res家庭画報.text, 'html.parser')
+soupRsk = BeautifulSoup(resRsk.text, 'html.parser')
+soupハルメク365 = BeautifulSoup(resハルメク365.text, 'html.parser')
+soup朝日新聞 = BeautifulSoup(res朝日新聞.text, 'html.parser')
+
+#タグの文字列を取得する
+txtConstellationLINE = soupLINE.find(class_='zodiac-item_name__c4Ypu')
+txtDetailLINE = soupLINE.find(class_='zodiac-item_summary__E_Vh7')
+# txtConstellationDmenu = soupDmenu.find(class_='postRank__infoHead')
+# txtDetailDmenu = soupDmenu.find(class_='postRank__infoFoot')
+txtConstellationIzumi = soupIzumi.find(class_='topSeizaName')
+txtDetailIzumi = soupIzumi.find(class_='seizaRstTxt')
+txtConstellation家庭画報 = soup家庭画報.find(class_='horoscope_name font_min')
+txtDetail家庭画報 = soup家庭画報.find(class_='horoscope_txt font_hk')
+txtConstellationRsk = soupRsk.find(class_='uranai_seiza')
+txtDetailRsk = soupRsk.find(class_='uranai_desc')
+txtConstellationハルメク365 = soupハルメク365.find(class_='sign-name')
+txtDetailハルメク365 = soupハルメク365.find(class_='text')
+txtConstellation朝日新聞 = soup朝日新聞.find(href='/uranai/12seiza/sagittarius.html')
+
+# print
+print(txtConstellationLINE)
+print(txtDetailLINE)
+# print(txtConstellationDmenu)
+# print(txtDetailDmenu)
+print(txtConstellationIzumi)
+print(txtDetailIzumi)
+print(txtConstellation家庭画報)
+print(txtDetail家庭画報)
+print(txtConstellationRsk)
+print(txtDetailRsk)
+print(txtConstellationハルメク365)
+print(txtDetailハルメク365)
+print(txtConstellation朝日新聞)
